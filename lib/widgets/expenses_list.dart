fd62c664c8bddf0072d7f328a4d8a39496b440ee
Thomas Wolfe <tjwolfe91@gmail.com>
2023-07-12 16:03:30 -0400

    Update expenses_list.dart

--- lib/widgets/expenses_list.dart
+++ lib/widgets/expenses_list.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({
-    super.key,
-    required this.expenses,
-  });
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +15,14 @@ class ExpensesList extends StatelessWidget {
     // creates list on demand.. only when items are scrollable they're loaded. Much more efficient
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      // Dismissible enables items to be swiped off / deleted
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
