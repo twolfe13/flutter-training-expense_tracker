6ea1eed339491d7e463baa7d4369d2578f0a2c68
Thomas Wolfe <tjwolfe91@gmail.com>
2023-07-12 16:04:40 -0400

    Update expenses.dart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,17 +30,63 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     ); // context is provided behind the scenes because it's part of a State
+  }
+
+  // executed above
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  // executed in espenses_list  makes that dismissible() removes the data internally.. not just from the screen
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(
+        expense); // .indexOf gives us the position of the value in the list
+
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            // if undo is pressed
+            setState(() {
+              _registeredExpenses.insert(expenseIndex,
+                  expense); // insert adds item to a certain position
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
-        backgroundColor: Colors.green,
+        backgroundColor: const Color.fromARGB(255, 224, 86, 5),
         actions: [
           IconButton(
             onPressed: _openAddExpenseOverlay,
@@ -53,7 +99,7 @@ class _ExpensesState extends State<Expenses> {
           // Toolbar with the Add button => Row()
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: mainContent,
           ), // Expanded()
         ],
       ),
